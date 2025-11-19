ebcbfecf956029b3e780ec9acd3e1079cd88b664
Miltonvaz <233427@ids.upchiapas.edu.mx>
2025-11-18 20:38:10 -0600

    first commit

---
+++ backend1/server.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+const port = 8088;
+
+app.get('/', (req, res) => {
+res.send(`Respuesta del Servidor ${req.hostname}:${port}`);
+});
+app.listen(port, () => {
+console.log(`Servidor escuchando en puerto ${port}`);
+});
